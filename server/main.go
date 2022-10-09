5788601df1b52d7b0e02f9a9c9418b8057c84681
FuenRob <fuenrob@gmail.com>
2022-10-09 14:06:34 +0200

    Add login restrictions to services

--- server/main.go
+++ server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	controllers "github.com/fuenrob/server-go/controllers"
+	model "github.com/fuenrob/server-go/models"
 	utils "github.com/fuenrob/server-go/utils"
-	"github.com/labstack/echo"
+	echo "github.com/labstack/echo/v4"
+	middleware "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -12,13 +14,21 @@ func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.POST("/login", controllers.Login(db))
 
-	e.GET("/users", controllers.GetAllUsers(db))
-	e.GET("/users/:id", controllers.GetUserById(db))
+	r := e.Group("/admin")
+
+	// Configure middleware with the custom claims type
+	r.Use(middleware.JWTWithConfig(model.GetConfigJWT()))
+
+	r.GET("/users", controllers.GetAllUsers(db))
+	r.GET("/users/:id", controllers.GetUserById(db))
 	e.POST("/users", controllers.CreateNewUser(db))
-	e.PUT("/users/:id", controllers.UpdateUserById(db))
-	e.DELETE("/users/:id", controllers.DeleteUserById(db))
+	r.PUT("/users/:id", controllers.UpdateUserById(db))
+	r.DELETE("/users/:id", controllers.DeleteUserById(db))
 
 	e.Logger.Fatal(e.Start(":9999"))
 }
